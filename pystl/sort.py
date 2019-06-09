5e9faa1f7c89b4880ac87521ebe0ef3b3f4ad4eb
MapleCCC <littlelittlemaple@gmail.com>
2019-06-09 20:48:54 +0800

    Add quick_sort algorithm implementation and unit test.

---
+++ pystl/sort.py
@@ -0,0 +1,58 @@
+from collections import Iterable
+
+
+def quick_sort(array: Iterable):
+    """
+    sort in-place.
+    descendance ordering.
+    unstable. Doesn't preserve ordering.
+    Raise TypeError if elemenets in array are non-comparable.
+    Raise ValueError if the subject is not iterable.
+    """
+
+    if not isinstance(array, Iterable):
+        raise ValueError("Quicksort algorithm only accepts list as argument.")
+
+    n = len(array)
+
+    if n == 0 or n == 1:
+        return array
+
+    pivot = array[0]
+    p = 1
+    q = n - 1
+
+    while True:
+        while array[q] >= pivot and p < q:
+            q -= 1
+        while array[p] < pivot and p < q:
+            p += 1
+        if p == q:
+            break
+        else:
+            array[p], array[q] = (array[q], array[p])
+
+    if pivot <= array[p]:
+        return [pivot] + quick_sort(array[1:])
+    else:
+        return quick_sort(array[1:p+1]) + [pivot] + quick_sort(array[p+1:])
+
+
+def bucket_sort(array):
+    pass
+
+
+def select_sort(array):
+    pass
+
+
+def heap_sort(array):
+    pass
+
+
+def bubble_sort(array):
+    return array
+
+
+sorting_algorithms = [quick_sort, select_sort,
+                      heap_sort, bubble_sort, bucket_sort]
---
+++ pystl/test/test_sort.py
@@ -0,0 +1,75 @@
+from ..sort import *
+import unittest
+from collections import Counter
+
+
+class TestSort(unittest.TestCase):
+    # def __init__(self, sorting_algorithm):
+    #     super().__init__()
+    #     self.sorting_algorithm = sorting_algorithm
+
+    # @classmethod
+    # def setUpClass(cls):
+    #     cls.sorting_algorithm = lambda x: x
+
+    sorting_algorithm = quick_sort
+
+    def test_empty_array(self):
+        self.verify([])
+
+    def test_single_element_array(self):
+        self.verify([1])
+
+    def test_trivial_sort(self):
+        self.verify([1, 5, 2, 7])
+
+    def test_duplicate_element(self):
+        self.verify([8, 5, 11, 7, 5])
+
+    def test_zero(self):
+        self.verify([1, 6, 4, 0])
+
+    def test_negative_number(self):
+        self.verify([8, 5, 11, 7, -1])
+
+    def test_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            self.__class__.sorting_algorithm(1)
+
+    def test_incomparable(self):
+        with self.assertRaises(TypeError):
+            self.__class__.sorting_algorithm([dict(), dict()])
+
+    def verify(self, array):
+        self._formally_verify_sorting_result(
+            self.__class__.sorting_algorithm, array)
+
+    def _formally_verify_sorting_result(self, sorting_algorithm, array):
+        input = array
+        output = sorting_algorithm(array)
+
+        self.assertEqual(Counter(input), Counter(output))
+
+        for i in range(len(output) - 1):
+            self.assertLessEqual(output[i], output[i + 1])
+
+
+class TestQuickSort(TestSort):
+    sorting_algorithm = quick_sort
+
+@unittest.skip("Not yet implemented")
+class TestSelectSort(TestSort):
+    sorting_algorithm = select_sort
+
+
+@unittest.skip("Not yet implemented")
+class TestHeapSort(TestSort):
+    sorting_algorithm = heap_sort
+
+
+# suite = unittest.TestSuite()
+# suite.addTest(TestQuickSort())
+
+# unittest.TextTestRunner().run(suite)
+
+# unittest.main()
