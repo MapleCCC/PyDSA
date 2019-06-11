e98283e6f9b0352d7ebe0d52f1aa6afa24f91e4a
MapleCCC <littlelittlemaple@gmail.com>
2019-06-12 02:19:11 +0800

    Refactor SplayTree implementation

--- pystl/splay_tree.py
+++ pystl/splay_tree.py
@@ -1,25 +1,108 @@
-__all__ = ['SplayTree']
+__all__ = ["SplayTree"]
 
-from .splay_tree_impl import SplayTree
+from .bst import BinarySearchTree
 
 
-class SplayTreeStub:
-    def __init__(self):
-        pass
+LEFT = 1
+RIGHT = 0
+
+
+class SplayTreeImpl1(BinarySearchTree):
+    def insert(self, key, value=None):
+        super().insert(key, value)
+        self.splay(key)
 
     def find(self, key):
-        return None
+        result = super().find(key)
+        if result is not None:
+            self.splay(key)
+        return result
 
-    def insert(self, key, value):
-        pass
+    def splay(self, key):
+        bookkeep = self.track(key)
+        if len(bookkeep) == 1:
+            return
+        if bookkeep[-1] is None:
+            bookkeep = bookkeep[:-2]
+        self._splay_helper(bookkeep)
 
-    @property
-    def size(self):
-        return 0
+    def _splay_helper(self, bookkeep):
+        if len(bookkeep) == 1:
+            return
+        if len(bookkeep) == 3:
+            self.single_rotate(bookkeep)
+            return
+        self.double_rotate(bookkeep)
+        self._splay_helper(bookkeep[:-5])
 
-    def prune(self):
-        pass
+    def single_rotate(self, bookkeep):
+        self.zig(*bookkeep[-3:])
+
+    def double_rotate(self, bookkeep):
+        branch1 = bookkeep[-4]
+        branch2 = bookkeep[-2]
+        if branch1 == branch2:
+            self.zig_zig(*bookkeep[-5:])
+        else:
+            self.zig_zag(*bookkeep[-5:])
+
+    def zig_zag(self, node1, branch1, node2, branch2, node3):
+        self.zig(node2, branch2, node3)
+        self.zig(node1, branch1, node2)
+
+    def zig_zig(self, node1, branch1, node2, branch2, node3):
+        self.zig(node1, branch1, node2)
+        self.zig(node1, branch2, node3)
+
+    def zig(self, node1, branch, node2):
+        if branch == LEFT:
+            node1.key, node2.key = (node2.key, node1.key)
+            node1.value, node2.value = (node2.value, node1.value)
+            node1.left = node2.left
+            node2.left = node2.right
+            node2.right = node1.right
+            node1.right = node2
+            return
+        if branch == RIGHT:
+            node1.key, node2.key = (node2.key, node1.key)
+            node1.value, node2.value = (node2.value, node1.value)
+            node1.right = node2.right
+            node2.right = node2.left
+            node2.left = node1.left
+            node1.left = node2
+            return
+
+    def _track(self, node, key, bookkeep):
+        if key == node.key:
+            bookkeep.append(node)
+            return
+
+        if key < node.key:
+            bookkeep += [node, LEFT]
+            if node.left is None:
+                bookkeep.append(None)
+                return
+            self._track(node.left, key, bookkeep)
+            return
+
+        if key > node.key:
+            bookkeep += [node, RIGHT]
+            if node.right is None:
+                bookkeep.append(None)
+                return
+            self._track(node.right, key, bookkeep)
+            return
+
+    def track(self, key):
+        bookkeep = []
+        if self.root is None:
+            bookkeep.append(None)
+        self._track(self.root, key, bookkeep)
+        return bookkeep
 
 
-# class SplayTree(SplayTreeStub):
-#     pass
+class SplayTreeImpl2(BinarySearchTree):
+    pass
+
+
+SplayTree = SplayTreeImpl1
--- pystl/test/test_splay_tree.py
+++ pystl/test/test_splay_tree.py
@@ -1,59 +1,55 @@
-from ..splay_tree_impl import SplayTree
+from ..splay_tree import SplayTree
 import unittest
-import pysnooper
-import os
 
 
 class SplayTreeTest(unittest.TestCase):
-    def test_insert(self):
-        st = SplayTree()
-        st.insert(1, 13)
-        # print(st)
-        self.assertEqual(st.find(1), 13)
+    def setUp(self):
+        self.st = SplayTree()
 
-    # @pysnooper.snoop(os.path.join(os.path.dirname(__file__), 'test_deletion.log'))
+    def tearDown(self):
+        del self.st
+
+    def test_insert(self):
+        self.st.insert(1, 13)
+        # print(self.st)
+        self.assertEqual(self.st.find(1), 13)
+
     def test_deletion(self):
-        st = SplayTree()
-        st.insert(1, 13)
-        st.delete(1)
-        self.assertEqual(st.size, 0)
+        self.st.insert(1, 13)
+        self.st.delete(1)
+        self.assertEqual(self.st.size, 0)
 
     def test_size(self):
-        st = SplayTree()
-        self.assertEqual(st.size, 0)
-        st.insert(1, 13)
-        self.assertEqual(st.size, 1)
-        st.insert(2, 13)
-        self.assertEqual(st.size, 2)
+        self.assertEqual(self.st.size, 0)
+        self.st.insert(1, 13)
+        self.assertEqual(self.st.size, 1)
+        self.st.insert(2, 13)
+        self.assertEqual(self.st.size, 2)
 
     def test_find(self):
-        st = SplayTree()
-        st.insert(1, 13)
-        self.assertEqual(st.find(2), None)
-        self.assertEqual(st.find(1), 13)
-        # self.assertEqual(st.find('a'), None)
+        self.st.insert(1, 13)
+        self.assertEqual(self.st.find(2), None)
+        self.assertEqual(self.st.find(1), 13)
+        # self.assertEqual(self.st.find('a'), None)
 
     def test_splay(self):
-        st = SplayTree()
-        st.insert(1, 13)
-        self.assertEqual(st.node.key, 1)
-        st.insert(2, 14)
-        self.assertEqual(st.size, 2)
-        self.assertEqual(st.node.key, 2)
-        st.insert(5, 13)
-        self.assertEqual(st.node.key, 5)
+        self.st.insert(1, 13)
+        self.assertEqual(self.st.root.key, 1)
+        self.st.insert(2, 14)
+        self.assertEqual(self.st.size, 2)
+        self.assertEqual(self.st.root.key, 2)
+        self.st.insert(5, 13)
+        self.assertEqual(self.st.root.key, 5)
 
-    # @pysnooper.snoop(os.path.join(os.path.dirname(__file__), 'test_cut_by_half.log'))
-    def test_cut_by_half(self):
-        st = SplayTree()
-        st.insert(1, 13)
-        st.insert(2, 14)
-        st.insert(5, 13)
-        st.insert(8, 89)
-        # print(st)
-        st.cut_by_half()
-        # print(st)
-        self.assertEqual(st.size, 2)
+    # def test_cut_by_half(self):
+    #     self.st.insert(1, 13)
+    #     self.st.insert(2, 14)
+    #     self.st.insert(5, 13)
+    #     self.st.insert(8, 89)
+    #     # print(self.st)
+    #     self.st.cut_by_half()
+    #     # print(self.st)
+    #     self.assertEqual(self.st.size, 2)
 
 
 if __name__ == '__main__':
